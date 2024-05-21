cd77460bebad06128e1f612cae572aef3b7170b4
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-21 13:55:44 +0200

    Utilities class lib

---
+++ Utilities/Utility.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace Utilities
+{
+    public class Utility
+    {
+        private static Random random = new();
+
+        public static Color GetRandomColor()
+        {
+            return Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+        }
+
+        public static Point GetRandomLocationInParent(Size parentSize, Size childSize)
+        {
+            int maxX = parentSize.Width - childSize.Width;
+            int maxY = parentSize.Height - childSize.Height;
+
+            return new Point
+            {
+                X = random.Next(maxX),
+                Y = random.Next(maxY)
+            };
+            
+
+        }
+    }
+}
