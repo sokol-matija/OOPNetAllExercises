7cc56f4e941017c8b9d2c658224a51cb7996f89a
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-22 15:49:21 +0200

    Exercise04 - Task01 complete

---
+++ Exercise04/OOPExercise04Task01/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace OOPExercise04Task01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            toolStrip1 = new ToolStrip();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            foxToolStripMenuItem = new ToolStripMenuItem();
+            monkeyToolStripMenuItem = new ToolStripMenuItem();
+            tigerToolStripMenuItem = new ToolStripMenuItem();
+            toolStripButton1 = new ToolStripButton();
+            toolStripButton2 = new ToolStripButton();
+            toolStripButton3 = new ToolStripButton();
+            pictureBox1 = new PictureBox();
+            toolStrip1.SuspendLayout();
+            contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ContextMenuStrip = contextMenuStrip1;
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1, toolStripButton2, toolStripButton3 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 25);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { foxToolStripMenuItem, monkeyToolStripMenuItem, tigerToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(118, 70);
+            // 
+            // foxToolStripMenuItem
+            // 
+            foxToolStripMenuItem.Name = "foxToolStripMenuItem";
+            foxToolStripMenuItem.Size = new Size(117, 22);
+            foxToolStripMenuItem.Tag = "fox";
+            foxToolStripMenuItem.Text = "Fox";
+            // 
+            // monkeyToolStripMenuItem
+            // 
+            monkeyToolStripMenuItem.Name = "monkeyToolStripMenuItem";
+            monkeyToolStripMenuItem.Size = new Size(117, 22);
+            monkeyToolStripMenuItem.Tag = "monkey";
+            monkeyToolStripMenuItem.Text = "Monkey";
+            // 
+            // tigerToolStripMenuItem
+            // 
+            tigerToolStripMenuItem.Name = "tigerToolStripMenuItem";
+            tigerToolStripMenuItem.Size = new Size(117, 22);
+            tigerToolStripMenuItem.Tag = "tiger";
+            tigerToolStripMenuItem.Text = "Tiger";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(23, 22);
+            toolStripButton1.Text = "toolStripButton1";
+            toolStripButton1.Click += toolStripButton1_Click;
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(23, 22);
+            toolStripButton2.Text = "toolStripButton2";
+            toolStripButton2.Click += toolStripButton1_Click;
+            // 
+            // toolStripButton3
+            // 
+            toolStripButton3.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton3.Image = (Image)resources.GetObject("toolStripButton3.Image");
+            toolStripButton3.ImageTransparentColor = Color.Magenta;
+            toolStripButton3.Name = "toolStripButton3";
+            toolStripButton3.Size = new Size(23, 22);
+            toolStripButton3.Text = "toolStripButton3";
+            toolStripButton3.Click += toolStripButton1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 25);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(800, 425);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(toolStrip1);
+            Name = "Form1";
+            Text = "Form1";
+            KeyDown += Form1_Down;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripButton toolStripButton2;
+        private ToolStripButton toolStripButton3;
+        private PictureBox pictureBox1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem foxToolStripMenuItem;
+        private ToolStripMenuItem monkeyToolStripMenuItem;
+        private ToolStripMenuItem tigerToolStripMenuItem;
+    }
+}
