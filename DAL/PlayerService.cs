d28e8430e4f22fadfb658d3f9a92530c185fd70d
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-22 13:01:00 +0200

    Task01 complete

---
+++ DAL/PlayerService.cs
@@ -0,0 +1,27 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class PlayerService
+    {
+        private const string PATH = "players.txt";
+
+        public List<Player> GetPlayers()
+        {
+            List<Player> players = new List<Player>();
+
+            string[] lines = System.IO.File.ReadAllLines(PATH);
+            foreach (string line in lines)
+            {
+                players.Add(Player.Parse(line));
+            }
+
+            return players;
+        }
+    }
+}
