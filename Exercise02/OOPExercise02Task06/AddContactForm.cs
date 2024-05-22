a705c9a3d65c2ce3db8b0a63d47ae1f8c2d77998
sokol.matija <48289756+sokol-matija@users.noreply.github.com>
2024-05-22 10:55:28 +0200

    Task06 complete

---
+++ Exercise02/OOPExercise02Task06/AddContactForm.cs
@@ -0,0 +1,28 @@
+﻿using Model;
+
+namespace OOPExercise02Task06
+{
+    public partial class AddContactForm : Form
+    {
+        public AddContactForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddContactForm_Load(object sender, EventArgs e)
+        {
+            ActiveControl = tbFirstName;
+        }
+
+        public Contacts GetContact()
+        {
+            return new Model.Contacts
+            {
+                FirstName = tbFirstName.Text,
+                LastName = tbLastName.Text,
+                Email = tbEmail.Text,
+                Phone = tbGSM.Text
+            };
+        }
+    }
+}
